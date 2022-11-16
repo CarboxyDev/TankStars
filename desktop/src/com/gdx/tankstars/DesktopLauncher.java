b1dfd58cfc54109014b292599079a408df383486
Arman <37100541+CarboxyDev@users.noreply.github.com>
2022-11-17 03:14:06 +0530

    Setup libgdx project

---
+++ desktop/src/com/gdx/tankstars/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package com.gdx.tankstars;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.gdx.tankstars.TankStarsGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setIdleFPS(60);
+		config.useVsync(true);
+		config.setWindowedMode(1920, 1080);
+		config.setTitle("Tank Stars");
+		new Lwjgl3Application(new TankStarsGame(), config);
+	}
+}
