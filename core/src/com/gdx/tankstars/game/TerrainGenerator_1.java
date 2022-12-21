9ac6a8bb6bdb049f8bb0c833b2dc026460c4c83b
arbaaz <arbaaz21034@iiitd.ac.in>
2022-12-21 14:57:41 +0530

    Update TerrainGenerator_1.java

--- core/src/com/gdx/tankstars/game/TerrainGenerator_1.java
+++ core/src/com/gdx/tankstars/game/TerrainGenerator_1.java
@@ -1,6 +1,10 @@
 package com.gdx.tankstars.game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Bezier;
 import com.badlogic.gdx.math.Vector2;
@@ -10,12 +14,14 @@ import javax.swing.text.AbstractDocument;
 
 
 public class TerrainGenerator_1 implements Screen {
-
+    SpriteBatch batch;
+    Texture texture;
     ShapeRenderer shapeRenderer;
     World world;
     Body body;
+    Body bodym;
+    Sprite sprite;
 
-    Body mover;
     Bezier<Vector2> bezierCurve;
     Box2DDebugRenderer debugRenderer;
 
@@ -35,10 +41,10 @@ public class TerrainGenerator_1 implements Screen {
         bodyDef.position.set(100, 100);
         PolygonShape shape = new PolygonShape();
         Vector2[] vertices = new Vector2[4];
-        vertices[0] = new Vector2(100, 100);
-        vertices[1] = new Vector2(200, 50);
-        vertices[2] = new Vector2(300, 150);
-        vertices[3] = new Vector2(400, 100);
+        vertices[0] = new Vector2(0, 200);
+        vertices[1] = new Vector2(400, 400);
+        vertices[2] = new Vector2(800, 150);
+        vertices[3] = new Vector2(1280, 420);
         shape.set(vertices);
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = shape;
@@ -48,20 +54,42 @@ public class TerrainGenerator_1 implements Screen {
         body = world.createBody(bodyDef);
         body.createFixture(fixtureDef);
 
-        BodyDef moverDef = new BodyDef();
-        moverDef.type = BodyDef.BodyType.DynamicBody;
-        moverDef.position.set(100, 100);
-        CircleShape moverShape = new CircleShape();
-        shape.setRadius(10);
-        FixtureDef moverfixtureDef = new FixtureDef();
-        moverfixtureDef.density = 1.0f;
-        moverfixtureDef.friction = 0.5f;
-        moverfixtureDef.restitution = 0.5f;
-        mover = world.createBody(bodyDef);
-        mover.createFixture(fixtureDef);
+        // Define the shape and properties of the body
+        BodyDef bodyDef1 = new BodyDef();
+        bodyDef1.type = BodyDef.BodyType.DynamicBody;
+        bodyDef1.position.set(400, 400);
+
+// Create the shape for the body
+        PolygonShape shape1 = new PolygonShape();
+        shape1.setAsBox(35, 25);
+
+// Define the body's properties
+        FixtureDef fixtureDef1 = new FixtureDef();
+        fixtureDef1.shape = shape1;
+        fixtureDef1.density = 1.0f;
+        fixtureDef1.friction = 0.5f;
+        fixtureDef1.restitution = 0.5f;
+
+        bodym = world.createBody(bodyDef1);
+        bodym.createFixture(fixtureDef1);
+
+        // Create a SpriteBatch to draw the image
+        batch = new SpriteBatch();
+
+// Load the PNG image
+        texture = new Texture(Gdx.files.internal("tanks/beast.png"));
+
+// Create a Sprite for the image
+        sprite = new Sprite(texture);
+        sprite.setSize(75,50);
+
+// Set the Sprite's position to the body's position
+        Vector2 pos = bodym.getPosition();
+        sprite.setPosition(pos.x, pos.y);
+
+
+
         debugRenderer = new Box2DDebugRenderer();
-
-
 
 
     }
@@ -84,11 +112,41 @@ public class TerrainGenerator_1 implements Screen {
             shapeRenderer.line(prev.x, prev.y, curr.x, curr.y);
             prev = curr;
         }
+
         shapeRenderer.end();
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            // Move the bodym to the left
+            bodym.setLinearVelocity(-100, 0);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            // Move the bodym to the right
+            bodym.setLinearVelocity(100, 0);
+        }
+            else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            // Move the bodym to the right
+            bodym.setLinearVelocity(0, -100);
+        }
+            else if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+                // Move the bodym to the right
+                bodym.setLinearVelocity(0, 100);
+            }
+        else {
+            // Stop the bodym if no keys are pressed
+            bodym.setLinearVelocity(0, 0);
+        }
+
+        // Set the Sprite's position to the body's position
+        Vector2 pos = bodym.getPosition();
+        sprite.setPosition(pos.x, pos.y);
+        bodym.setTransform(pos, body.getAngle());
+
         Camera camera = new Camera();
+
+        batch.begin();
+        sprite.draw(batch);
+        batch.end();
         // Render the physics body using Box2D DebugRenderer
         debugRenderer.render(world, camera.combined);
-
 
 
 
